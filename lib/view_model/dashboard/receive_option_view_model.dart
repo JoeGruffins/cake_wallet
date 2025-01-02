003f71f357eb418b8413e9f793c2041f4838ab39
Wisdom Arerosuoghene <wisdom.arerosuoghene@gmail.com>
2025-01-02 12:54:56 +0100

    decred testnet

--- lib/view_model/dashboard/receive_option_view_model.dart
+++ lib/view_model/dashboard/receive_option_view_model.dart
@@ -6,32 +6,48 @@ import 'package:mobx/mobx.dart';
 
 part 'receive_option_view_model.g.dart';
 
-class ReceiveOptionViewModel = ReceiveOptionViewModelBase with _$ReceiveOptionViewModel;
+class ReceiveOptionViewModel = ReceiveOptionViewModelBase
+    with _$ReceiveOptionViewModel;
 
 abstract class ReceiveOptionViewModelBase with Store {
   ReceiveOptionViewModelBase(this._wallet, this.initialPageOption)
       : selectedReceiveOption = initialPageOption ??
             (_wallet.type == WalletType.bitcoin ||
-             _wallet.type == WalletType.litecoin
+                    _wallet.type == WalletType.litecoin
                 ? bitcoin!.getSelectedAddressType(_wallet)
-                : ReceivePageOption.mainnet),
+                : (_wallet.type == WalletType.decred && _wallet.isTestnet)
+                    ? ReceivePageOption.testnet
+                    : ReceivePageOption.mainnet),
         _options = [] {
     final walletType = _wallet.type;
     switch (walletType) {
       case WalletType.bitcoin:
         _options = [
           ...bitcoin!.getBitcoinReceivePageOptions(),
-          ...ReceivePageOptions.where((element) => element != ReceivePageOption.mainnet)
+          ...ReceivePageOptions.where(
+              (element) => element != ReceivePageOption.mainnet)
         ];
         break;
       case WalletType.litecoin:
         _options = [
           ...bitcoin!.getLitecoinReceivePageOptions(),
-          ...ReceivePageOptions.where((element) => element != ReceivePageOption.mainnet)
+          ...ReceivePageOptions.where(
+              (element) => element != ReceivePageOption.mainnet)
         ];
         break;
       case WalletType.haven:
         _options = [ReceivePageOption.mainnet];
+        break;
+      case WalletType.decred:
+        if (_wallet.isTestnet) {
+          _options = [
+            ReceivePageOption.testnet,
+            ...ReceivePageOptions.where(
+                (element) => element != ReceivePageOption.mainnet)
+          ];
+        } else {
+          _options = ReceivePageOptions;
+        }
         break;
       default:
         _options = ReceivePageOptions;
